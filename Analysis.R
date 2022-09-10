ba2db21cac0cdf2d30da9f5fd902dc97ea7c1d19
Kang <kangjf1943@gmail.com>
2022-09-10 15:42:00 +0900

    fix: change directory of pics

--- Analysis.R
+++ Analysis.R
@@ -4,7 +4,7 @@ library(dplyr)
 # 爬取存放图片的网页中各文件对应的名字和链接
 node.content <- 
   # 获取网页信息
-  read_html("https://github.com/kangjf1943/SharePic/tree/main/new") %>% 
+  read_html("https://github.com/kangjf1943/SharePic/tree/main/NewPic") %>% 
   # 找到对应节点并且获取内容
   html_nodes("div.js-details-container span a.js-navigation-open")
 # 提取标题和链接
@@ -18,3 +18,5 @@ tibble(name = html_attr(x= node.content, name = "title"),
   mutate(link = paste0("https://github.com", link, "?raw=true")) %>% 
   # 导出表格
   openxlsx::write.xlsx("ProcData/Second_hand_pics.xlsx")
+
+# 在复制数据之前别忘记把图片路径中的“NewPic”换成“RawData”，并且把图片也都移动到“RawData”中去
